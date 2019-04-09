d4183807df4ba46114b5cd01b787812762a42683
bytecode77 <mail@martinfischer.it>
2019-04-09 11:39:11 +0200

    * CSharp.FindDataContext -> UIContext.Find

--- BytecodeApi/CSharp.cs
+++ BytecodeApi/CSharp.cs
@@ -6,7 +6,6 @@ using System.Collections.ObjectModel;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading;
-using System.Windows;
 
 namespace BytecodeApi
 {
@@ -379,23 +378,6 @@ namespace BytecodeApi
 		public static bool EqualsAll<T>(T obj, params T[] values)
 		{
 			return EqualsAll(obj, (IEnumerable<T>)values);
-		}
-
-		/// <summary>
-		/// Returns the DataContext property, if the specified <see cref="object" /> instance is either a <see cref="FrameworkElement" /> or a <see cref="FrameworkContentElement" />. If found and can be casted to the specified type, the DataContext is returned, otherwise, <see langword="default" />(<typeparamref name="T" />).
-		/// </summary>
-		/// <typeparam name="T">The return type to cast the DataContext property to.</typeparam>
-		/// <param name="obj">The <see cref="object" /> where the DataContext property looked for. This is typically a parameter from a WPF event handler.</param>
-		/// <returns>
-		/// <see cref="FrameworkElement.DataContext" />, if <paramref name="obj" /> is a <see cref="FrameworkElement" />;
-		/// <see cref="FrameworkContentElement.DataContext" />, if <paramref name="obj" /> instance is a <see cref="FrameworkContentElement" />;
-		/// otherwise, <see langword="default" />(<typeparamref name="T" />).
-		/// </returns>
-		public static T FindDataContext<T>(object obj)
-		{
-			if (obj is FrameworkElement frameworkElement && frameworkElement.DataContext is T dataContext1) return dataContext1;
-			else if (obj is FrameworkContentElement frameworkContentElement && frameworkContentElement.DataContext is T dataContext2) return dataContext2;
-			else return default;
 		}
 	}
 }
