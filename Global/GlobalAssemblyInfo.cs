ccd87ea13fe6ce83ded93d836d11a75104dd195a
Martin Fischer <mail@martinfischer.it>
2019-04-16 19:46:18 +0200

    1.2.2

--- Global/GlobalAssemblyInfo.cs
+++ Global/GlobalAssemblyInfo.cs
@@ -1,7 +1,7 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyVersion("1.2.1")]
-[assembly: AssemblyFileVersion("1.2.1")]
+[assembly: AssemblyVersion("1.2.2")]
+[assembly: AssemblyFileVersion("1.2.2")]
 [assembly: AssemblyCompany("bytecode77")]
 [assembly: AssemblyCopyright("© bytecode77, 2019.")]
 
