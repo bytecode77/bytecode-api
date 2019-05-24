5b5c02831293f715c418be3eb6dcd43b97dcf635
bytecode77 <mail@martinfischer.it>
2019-05-24 08:54:11 +0200

    -

--- BytecodeApi.UI/Controls/ContainerItemsControl.cs
+++ BytecodeApi.UI/Controls/ContainerItemsControl.cs
@@ -3,12 +3,12 @@
 namespace BytecodeApi.UI.Controls
 {
 	/// <summary>
-	/// Represents an <see cref="ItemsControl" /> that applies the <see cref="ItemsControl.ItemTemplate "/>, even if the item is its own container (e.g. a UserControl).
+	/// Represents an <see cref="ItemsControl" /> that applies the <see cref="ItemsControl.ItemTemplate" />, even if the item is its own container (e.g. a UserControl).
 	/// </summary>
 	public class ContainerItemsControl : ItemsControl
 	{
 		/// <summary>
-		/// Overwrites the IsItemItsOwnContainer property by always returning <see langword="false" />. This allows the <see cref="ItemsControl.ItemTemplate "/> to be also used with user controls.
+		/// Overwrites the IsItemItsOwnContainer property by always returning <see langword="false" />. This allows the <see cref="ItemsControl.ItemTemplate" /> to be also used with user controls.
 		/// </summary>
 		/// <param name="item">The item to check.</param>
 		/// <returns>
