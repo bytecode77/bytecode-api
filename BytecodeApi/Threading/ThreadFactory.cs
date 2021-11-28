f30b7b5ec6169d213e4bd1f868543b8fb4fb4879
bytecode77 <mail@martinfischer.it>
2021-11-28 21:24:04 +0100

    - BytecodeApi.Threading.Atomic removed

--- BytecodeApi/Threading/ThreadFactory.cs
+++ BytecodeApi/Threading/ThreadFactory.cs
@@ -7,7 +7,7 @@ using System.Windows.Threading;
 namespace BytecodeApi.Threading
 {
 	/// <summary>
-	/// Provides support for creating <see cref="Thread" /> and other multithreading related objects.
+	/// Provides support for creating <see cref="Thread" /> and other multi threading related objects.
 	/// </summary>
 	public static class ThreadFactory
 	{
