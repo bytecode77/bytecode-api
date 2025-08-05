419177ece91083a5edcd470d4ec8a6af6a4eff03
Martin Fischer <mail@martinfischer.it>
2025-08-05 16:58:38 +0200

    + BytecodeApi.Data.TimeoutAttribute

---
+++ BytecodeApi/Data/TimeoutAttribute.cs
@@ -0,0 +1,21 @@
+﻿namespace BytecodeApi.Data;
+
+/// <summary>
+/// Represents an attribute that specifies a timeout for an operation.
+/// </summary>
+public sealed class TimeoutAttribute : Attribute
+{
+	/// <summary>
+	/// Gets the timeout, in milliseconds.
+	/// </summary>
+	public int Milliseconds { get; private init; }
+
+	/// <summary>
+	/// Initializes a new instance of the <see cref="TimeoutAttribute" /> class.
+	/// </summary>
+	/// <param name="milliseconds">The timeout, in milliseconds.</param>
+	public TimeoutAttribute(int milliseconds)
+	{
+		Milliseconds = milliseconds;
+	}
+}
