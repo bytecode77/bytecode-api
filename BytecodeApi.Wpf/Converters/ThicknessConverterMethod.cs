702ec594913b09e310ef39a8cafceb217a2e837f
Martin Fischer <mail@martinfischer.it>
2025-08-26 15:01:55 +0200

    + BytecodeApi.Wpf.Converters.ThicknessConverter Add* methods

--- BytecodeApi.Wpf/Converters/ThicknessConverterMethod.cs
+++ BytecodeApi.Wpf/Converters/ThicknessConverterMethod.cs
@@ -8,43 +8,67 @@ namespace BytecodeApi.Wpf.Converters;
 public enum ThicknessConverterMethod
 {
 	/// <summary>
-	/// Returns a <see cref="Thickness" /> value, where <see cref="Thickness.Left" /> ist set to the parameter value.
+	/// Returns a <see cref="Thickness" /> value, where <see cref="Thickness.Left" /> is set to the parameter value.
 	/// </summary>
 	SetLeft,
 	/// <summary>
-	/// Returns a <see cref="Thickness" /> value, where <see cref="Thickness.Top" /> ist set to the parameter value.
+	/// Returns a <see cref="Thickness" /> value, where <see cref="Thickness.Top" /> is set to the parameter value.
 	/// </summary>
 	SetTop,
 	/// <summary>
-	/// Returns a <see cref="Thickness" /> value, where <see cref="Thickness.Right" /> ist set to the parameter value.
+	/// Returns a <see cref="Thickness" /> value, where <see cref="Thickness.Right" /> is set to the parameter value.
 	/// </summary>
 	SetRight,
 	/// <summary>
-	/// Returns a <see cref="Thickness" /> value, where <see cref="Thickness.Bottom" /> ist set to the parameter value.
+	/// Returns a <see cref="Thickness" /> value, where <see cref="Thickness.Bottom" /> is set to the parameter value.
 	/// </summary>
 	SetBottom,
 	/// <summary>
-	/// Returns a <see cref="Thickness" /> value, where <see cref="Thickness.Left" /> and <see cref="Thickness.Right" /> ist set to the parameter value.
+	/// Returns a <see cref="Thickness" /> value, where <see cref="Thickness.Left" /> and <see cref="Thickness.Right" /> is set to the parameter value.
 	/// </summary>
 	SetHorizontal,
 	/// <summary>
-	/// Returns a <see cref="Thickness" /> value, where <see cref="Thickness.Top" /> and <see cref="Thickness.Bottom" /> ist set to the parameter value.
+	/// Returns a <see cref="Thickness" /> value, where <see cref="Thickness.Top" /> and <see cref="Thickness.Bottom" /> is set to the parameter value.
 	/// </summary>
 	SetVertical,
 	/// <summary>
-	/// Returns a <see cref="Thickness" /> value, where every property except <see cref="Thickness.Left" /> ist set to the parameter value.
+	/// Returns a <see cref="Thickness" /> value, where <see cref="Thickness.Left" /> ist incremented by the parameter value.
+	/// </summary>
+	AddLeft,
+	/// <summary>
+	/// Returns a <see cref="Thickness" /> value, where <see cref="Thickness.Top" /> ist incremented by the parameter value.
+	/// </summary>
+	AddTop,
+	/// <summary>
+	/// Returns a <see cref="Thickness" /> value, where <see cref="Thickness.Right" /> ist incremented by the parameter value.
+	/// </summary>
+	AddRight,
+	/// <summary>
+	/// Returns a <see cref="Thickness" /> value, where <see cref="Thickness.Bottom" /> ist incremented by the parameter value.
+	/// </summary>
+	AddBottom,
+	/// <summary>
+	/// Returns a <see cref="Thickness" /> value, where <see cref="Thickness.Left" /> and <see cref="Thickness.Right" /> ist incremented by the parameter value.
+	/// </summary>
+	AddHorizontal,
+	/// <summary>
+	/// Returns a <see cref="Thickness" /> value, where <see cref="Thickness.Top" /> and <see cref="Thickness.Bottom" /> ist incremented by the parameter value.
+	/// </summary>
+	AddVertical,
+	/// <summary>
+	/// Returns a <see cref="Thickness" /> value, where every property except <see cref="Thickness.Left" /> is set to the parameter value.
 	/// </summary>
 	KeepLeft,
 	/// <summary>
-	/// Returns a <see cref="Thickness" /> value, where every property except <see cref="Thickness.Top" /> ist set to the parameter value.
+	/// Returns a <see cref="Thickness" /> value, where every property except <see cref="Thickness.Top" /> is set to the parameter value.
 	/// </summary>
 	KeepTop,
 	/// <summary>
-	/// Returns a <see cref="Thickness" /> value, where every property except <see cref="Thickness.Right" /> ist set to the parameter value.
+	/// Returns a <see cref="Thickness" /> value, where every property except <see cref="Thickness.Right" /> is set to the parameter value.
 	/// </summary>
 	KeepRight,
 	/// <summary>
-	/// Returns a <see cref="Thickness" /> value, where every property except <see cref="Thickness.Bottom" /> ist set to the parameter value.
+	/// Returns a <see cref="Thickness" /> value, where every property except <see cref="Thickness.Bottom" /> is set to the parameter value.
 	/// </summary>
 	KeepBottom,
 	/// <summary>
