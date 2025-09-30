e0212cb8f84c6b5fe2350b30b92acf8e8c158e86
Martin Fischer <mail@martinfischer.it>
2025-09-30 12:00:56 +0200

    * JsonConverters ParseExact attribute

--- BytecodeApi/Text/Json/Converters/JsonTimeOnlyAttribute.cs
+++ BytecodeApi/Text/Json/Converters/JsonTimeOnlyAttribute.cs
@@ -12,16 +12,29 @@ public class JsonTimeOnlyAttribute : JsonConverterAttribute
 	/// The format that is used to convert <see cref="TimeOnly" /> values.
 	/// </summary>
 	public string Format { get; private init; }
+	/// <summary>
+	/// A <see cref="bool" /> value indicating whether to require the exact format during read operations. The default value is <see langword="false" />, which allows any format during read operations.
+	/// </summary>
+	public bool ParseExact { get; private init; }
 
 	/// <summary>
 	/// Initializes a new instance of the <see cref="JsonTimeOnlyAttribute" /> class.
 	/// </summary>
 	/// <param name="format">The format that is used to convert <see cref="TimeOnly" /> values.</param>
-	public JsonTimeOnlyAttribute(string format)
+	public JsonTimeOnlyAttribute(string format) : this(format, false)
+	{
+	}
+	/// <summary>
+	/// Initializes a new instance of the <see cref="JsonTimeOnlyAttribute" /> class.
+	/// </summary>
+	/// <param name="format">The format that is used to convert <see cref="TimeOnly" /> values.</param>
+	/// <param name="parseExact"><see langword="true" /> to require the exact format; <see langword="false" /> to allow any format during read operations.</param>
+	public JsonTimeOnlyAttribute(string format, bool parseExact)
 	{
 		Check.ArgumentNull(format);
 
 		Format = format;
+		ParseExact = parseExact;
 	}
 
 	/// <summary>
@@ -33,6 +46,6 @@ public class JsonTimeOnlyAttribute : JsonConverterAttribute
 	/// </returns>
 	public override JsonConverter CreateConverter(Type typeToConvert)
 	{
-		return new TimeOnlyJsonConverter(Format);
+		return new TimeOnlyJsonConverter(Format, ParseExact);
 	}
 }
