93ca9e016619b7839ed71771e7e52c2893ba2921
Martin Fischer <mail@martinfischer.it>
2025-11-05 19:58:15 +0100

    + BytecodeApi.Wpf.Cui.Controls.UiWindow.TitleBarBrush

--- BytecodeApi.Wpf.Cui/Controls/UiWindow.cs
+++ BytecodeApi.Wpf.Cui/Controls/UiWindow.cs
@@ -1,4 +1,5 @@
 ﻿using BytecodeApi.Wpf.Controls;
+using BytecodeApi.Wpf.Extensions;
 using BytecodeApi.Wpf.Services;
 using System.Windows;
 using System.Windows.Media;
@@ -7,6 +8,13 @@ namespace BytecodeApi.Wpf.Cui.Controls;
 
 public class UiWindow : ObservableWindow
 {
+	public static readonly DependencyProperty TitleBarBrushProperty = DependencyPropertyEx.Register(nameof(TitleBarBrush), new(TitleBarBrush_Changed));
+	public Brush? TitleBarBrush
+	{
+		get => this.GetValue<Brush>(TitleBarBrushProperty);
+		set => SetValue(TitleBarBrushProperty, value);
+	}
+
 	static UiWindow()
 	{
 		DefaultStyleKeyProperty.OverrideMetadata(typeof(UiWindow), new FrameworkPropertyMetadata(typeof(UiWindow)));
@@ -23,6 +31,15 @@ public class UiWindow : ObservableWindow
 	{
 		UiWindow window = (UiWindow)dependencyObject;
 		window.UpdateBorder();
+	}
+	private static void TitleBarBrush_Changed(DependencyObject dependencyObject, DependencyPropertyChangedEventArgs e)
+	{
+		UiWindow window = (UiWindow)dependencyObject;
+
+		if (window.TitleBarBrush is SolidColorBrush titleBarBrush)
+		{
+			WindowService.SetTitleBarBrush(window, titleBarBrush);
+		}
 	}
 
 	private void UpdateBorder()
