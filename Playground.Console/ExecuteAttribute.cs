1017a88dd37c695323e6a3cca76eab5d70691fb4
Martin Fischer <mail@martinfischer.net>
2026-03-25 22:13:02 +0100

    + GenericRestClient: Expose protected properties

--- Playground.Console/ExecuteAttribute.cs
+++ Playground.Console/ExecuteAttribute.cs
@@ -1,7 +1,7 @@
 ﻿/// <summary>
 /// This method will be executed. The method must have either no parameters, or a <see cref="string" />[] parameter.
 /// </summary>
-[AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
+[AttributeUsage(AttributeTargets.Method)]
 public sealed class ExecuteAttribute : Attribute
 {
 }
